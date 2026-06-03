7e985a9caaecba1b6b5cf90ab9a78e5b524d30c1
Onur Acar <onuracar.work@gmail.com>
2026-06-03 23:02:06 +0300

    add : implement video playback functionality in VideoArkaPlan widget

--- ecotrack/lib/widgets/video_arka_plan.dart
+++ ecotrack/lib/widgets/video_arka_plan.dart
@@ -18,3 +18,50 @@ class VideoArkaPlan extends StatefulWidget {
     return _VideoArkaPlanState();
   }
 }
+
+class _VideoArkaPlanState extends State<VideoArkaPlan> {
+  late VideoPlayerController _kontrolcu;
+
+  @override
+  void initState() {
+    super.initState();
+    _kontrolcu = VideoPlayerController.asset(widget.videoYolu);
+    _kontrolcu.initialize().then((_) {
+      _kontrolcu.setLooping(true);
+      _kontrolcu.setVolume(0.0);
+      _kontrolcu.play();
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _kontrolcu.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        if (_kontrolcu.value.isInitialized)
+          FittedBox(
+            fit: BoxFit.cover,
+            alignment: widget.hizalama,
+            child: SizedBox(
+              width: _kontrolcu.value.size.width,
+              height: _kontrolcu.value.size.height,
+              child: VideoPlayer(_kontrolcu),
+            ),
+          )
+        else
+          Container(color: Colors.black),
+        Container(
+          color: Colors.black.withOpacity(0.5),
+        ),
+        widget.icerik,
+      ],
+    );
+  }
+}
